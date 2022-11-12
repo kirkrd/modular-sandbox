e5cfd16b3325636ab691fed010b1683a58878f80
Kristoffer Kirkerud <kirkerudk@gmail.com>
2022-11-12 11:44:57 +0100

    feat: finished example of webpack modular federation

--- container/src/App.tsx
+++ container/src/App.tsx
@@ -1,6 +1,5 @@
-mport { Box, Center, Flex, Heading, Spinner, Image, Link, Text } from '@chakra-ui/react';
+import { Box, Center, Flex, Heading, Spinner, Image, Link, Text } from '@chakra-ui/react';
 import React from 'react';
-
 const CounterAppOne = React.lazy(() => import('app1/CounterAppOne'));
 const CounterAppTwo = React.lazy(() => import('app2/CounterAppTwo'));
 
@@ -15,7 +14,7 @@ const App = () => (
       flexDirection="column"
     >
       <Box color="#fff" position="fixed" right="0" top="0" mr="2rem" mt="2rem">
-        Latest Build Date: <Text fontWeight="bold">{version}</Text>
+        Latest Build Date: <Text fontWeight="bold">{process.env.npm_package_version}</Text>
       </Box>
       <Flex
         border="1px solid #151421"
@@ -53,13 +52,6 @@ const App = () => (
           </React.Suspense>
         </Flex>
       </Flex>
-      <Link
-        marginTop="5rem"
-        href="https://github.com/ogzhanolguncu/react-typescript-module-federation"
-        target="_blank"
-      >
-        <Image src="./git.png" height="45px" width="45px" />
-      </Link>
     </Center>
   </>
 );
